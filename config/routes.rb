aac41c0912085ffe8025ba534b515d8dee17ffde
said.montufar <said.montufar@outlook.com>
2023-09-05 22:16:17 -0400

    Add patch route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
+# Verb | URL =>
   root "movies#index"
   get "movies" => "movies#index"
   get "movies/:id" => "movies#show", as: "movie"
   get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  patch "movies/:id" => "movies#update"
 end
