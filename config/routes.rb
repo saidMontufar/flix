ab17f2cc5f8879e272d94cf06651948cbdfce398
said.montufar <said.montufar@outlook.com>
2023-09-06 23:16:05 -0400

    Add resources controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
-# Verb | URL =>
   root "movies#index"
-  get "movies" => "movies#index"
-  get "movies/:id" => "movies#show", as: "movie"
-  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
-  patch "movies/:id" => "movies#update"
+
+  resources :movies
 end
