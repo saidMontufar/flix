81841fbc044024320f79f56ecc26e6349e5da1bc
said.montufar <said.montufar@outlook.com>
2023-09-08 21:51:21 -0400

    Add custom query

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,11 @@
 class Movie < ApplicationRecord
+
+  def self.released
+    where("released_on < ?", Time.now).
+      order("released_on asc")
+  end
+
+
   def flop?
     total_gross.blank? || total_gross < 25_000_000
   end
