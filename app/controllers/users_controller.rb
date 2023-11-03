fcb1deed6665b85bce99098b6a59239f515a75f4
said.montufar <said.montufar@outlook.com>
2023-11-02 22:13:39 -0400

    Add signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new()
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user, notice: "Account was successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).
+      permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  get "signup" => "users#new"
+
 end
