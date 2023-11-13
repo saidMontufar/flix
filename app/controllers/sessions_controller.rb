dc775a2e73762a8fa0d17c109541da79e53fca91
said.montufar <said.montufar@outlook.com>
2023-11-12 19:32:05 -0500

    Add create session action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    fail
+    user = User.find_by(email: params[:email])
+    user.authenticate(params[:password])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back #{user.name}!"
+    else
+      flash.now[:alert] = "Invalid email/password combination!"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
