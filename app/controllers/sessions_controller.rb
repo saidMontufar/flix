b36f6e6ebba42e3c764ec7e6e07d76184a2ef6c6
said.montufar <said.montufar@outlook.com>
2023-11-13 19:19:30 -0500

    Add current user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to movies_url, status: :see_other,
+               notice: "Good bye!"
 
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: "Account was successfully created!"
     else
       render :new, status: :unprocessable_entity
@@ -40,6 +41,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
+    session[:user_id] = nil
 
     redirect_to movies_url, status: :see_other,
         alert: "Account succesfully deleted!"
