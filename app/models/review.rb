b43b1bcf1ce6e05fa28834251615ab60b70eba89
said.montufar <said.montufar@outlook.com>
2023-11-15 22:48:03 -0500

    Add many to many associations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,13 +1,13 @@
 class Review < ApplicationRecord
   belongs_to :movie
+  belongs_to :user
 
-  validates :name, presence: true
   validates :comment, length: { minimum: 4 }
 
   STARS = [1,2,3,4,5]
   validates :stars, inclusion: {
-                                  in: STARS,
-                                  message: "Must be between 1 and 5"
-                                }
+            in: STARS,
+            message: "Must be between 1 and 5"
+}
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  has_many :reviews, dependent: :destroy
+
   has_secure_password
 
   validates :name, presence: true
