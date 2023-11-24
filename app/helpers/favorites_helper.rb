ead66c719eb0bba3827f1f0d3666ec975a81b38a
said.montufar <said.montufar@outlook.com>
2023-11-23 20:43:18 -0500

    Add more threugh associations

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,2 +1,11 @@
 module FavoritesHelper
+
+  def fave_or_unfave_button(movie, favorite)
+    if favorite
+     button_to "⚝ Unfave!", movie_favorite_path(@movie, @favorite),
+          method: :delete
+    else
+      button_to "🌟 Fave!", movie_favorites_path(@movie)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :favorites
   resources :users
 
   root "movies#index"
 
   resources :movies do
     resources :reviews
+    resources :favorites, only: [:create, :destroy]
   end
 
   resource :session, only: [:new, :create, :destroy]
